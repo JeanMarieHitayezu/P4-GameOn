5a5828ffffc4f7044e07382f8f3e7fcd10dd442c
Jean Marie Hitayezu <hitayezujeanmarie@gmail.com>
2022-02-18 11:13:54 +0100

    Correction on index.html and modal.js

--- JavaScript/modal.js
+++ JavaScript/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalCloseBtn = document.getElementsByClassName('close');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +20,13 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// Close modal form
+function closeiModal() {
+  modalbg.style.display="none";
+}
+
+// Close modal event
+modalCloseBtn[0].addEventListener('click', closeModal);
 
 
